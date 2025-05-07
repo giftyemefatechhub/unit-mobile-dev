653c68874847ac99a151fa78321fa61c92665746
giftyemefatechhub <giftyemefaakaglah@gmail.com>
2025-05-08 00:56:40 +0200

    Added Home Dashboard integration and routing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,17 @@ android {
 }
 
 dependencies {
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v350)
+    androidTestImplementation(libs.ui.test.junit4)
+    testImplementation(libs.junit)
+
 
     implementation(libs.androidx.core.ktx)
-    implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -53,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.test.junit4.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
